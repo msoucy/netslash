1bd97546b2fd57c6485a06c5dbe97c0d4646820a
Matt Soucy <msoucy@csh.rit.edu>
2013-01-19 01:42:56 -0500

    Added the start of the JSON serialization for the board

--- src/netslash/core/board.d
+++ src/netslash/core/board.d
@@ -1,5 +1,9 @@
 module netslash.core.board;
 
+import std.json;
+import std.algorithm;
+import std.array;
+
 import netslash.core.tile;
 
 class Board {
@@ -7,4 +11,25 @@ class Board {
 	enum MAX_COLS = 15;
 	Tile[MAX_ROWS][MAX_COLS] board;
 	alias this = board;
+
+	string serialize() {
+		JSONValue b;
+		b.type = JSON_TYPE.OBJECT;
+		b.object["rows"] = JSONValue();
+		b.object["rows"].type = JSON_TYPE.UINTEGER;
+		b.object["rows"].uinteger = MAX_ROWS;
+		b.object["cols"] = JSONValue();
+		b.object["cols"].type = JSON_TYPE.UINTEGER;
+		b.object["cols"].uinteger = MAX_COLS;
+		b.object["board"] = JSONValue();
+		b.object["board"].type = JSON_TYPE.STRING;
+		Appender!(char[]) appr;
+		foreach(row;board) {
+			foreach(tile; row) {
+				appr.put(tile.rep);
+			}
+		}
+		b.object["board"].str;
+		return b.toJSON();
+	}
 }

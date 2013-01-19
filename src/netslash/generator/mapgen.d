53954ebb5621eef989f8f4c6340f65da7441e9b4
Andrew Hanes <ahanes@csh.rit.edu>
2013-01-19 05:07:14 -0500

    debugging stuff added

--- src/netslash/generator/mapgen.d
+++ src/netslash/generator/mapgen.d
@@ -10,21 +10,24 @@ import std.random;
 import netslash.core.stairs;
 
 Board genTestMap() {
+    debug {writefln("generating board"); }
     Board b = new Board();
     //Gen upStaris
     int a = cast(int)uniform(1, b.length - 1);
     int aa = cast(int)uniform(1, b[0].length - 1);
+
+    debug {writefln("den up"); }
     b.board[a][aa] = new UpStairs();
     b.board[a][aa].x = a;
     b.board[a][aa].y = aa;
 
-
+    debug {writefln("gen downs");}
     //Gen downstairs
     a = cast(int)uniform(1, b.length-1);
     aa = cast(int)uniform(1, b[0].length-1);
     b.board[a][aa] = new DownStairs();
     b.board[a][aa].x = a;
     b.board[a][aa].y = aa;
-
+    b.print();
     return b;
 }

633c8f63750c221d7f5b0b57f1f12901078f60f2
Andrew Hanes <ahanes@csh.rit.edu>
2013-01-19 03:05:29 -0500

    Randomly generated stairs added

--- src/netslash/generator/mapgen.d
+++ src/netslash/generator/mapgen.d
@@ -4,6 +4,8 @@ import netslash.core.tile;
 import netslash.core.board;
 import std.conv;
 import std.string;
+import std.random;
+import netslash.core.stairs;
 
 Board genTestMap() {
     Board b = new Board();
@@ -21,8 +23,24 @@ Board genTestMap() {
             b.board[i][n].y = n;
         }
     }
+    //Gen upStaris
+    int a = cast(int)uniform(1, b.length - 1);
+    int aa = cast(int)uniform(1, b[0].length - 1);
+    b.board[a][aa] = new UpStairs();
+    b.board[a][aa].x = a;
+    b.board[a][aa].y = aa;
+
+
+    //Gen downstairs
+    a = cast(int)uniform(1, b.length-1);
+    aa = cast(int)uniform(1, b[0].length-1);
+    b.board[a][aa] = new DownStairs();
+    b.board[a][aa].x = a;
+    b.board[a][aa].y = aa;
+
     return b;
 }
+
 int main(char[][] argv) {
     writeln("Generating map...");
     auto f = File("Maps/map.nsmap", "w");

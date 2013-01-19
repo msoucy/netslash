4260aea81ffa97c7bf31aeed7bb380d573220dfc
Frank Hrach IV <fhrach4@gmail.com>
2013-01-19 00:41:57 -0500

    Added unarmed.d and modified player with some more detalied feilds

---
+++ src/netslash/core/unarmed.d
@@ -0,0 +1,34 @@
+module netslash.core.unarmed;
+
+import netslash.core.weapon;
+
+class Unarmed : Weapon
+{
+	private:
+	public:
+
+	/**
+	 * Create a new unarmed weapon.
+	 * Unarmed always does +0 damage
+	 **/
+	public this()
+	{
+		BASE_DAMAGE = 0;
+	}
+
+	public:
+		override getCharacter()
+		{
+			return ' ';
+		}
+
+		override getName()
+		{
+			return "Fisticuffs";
+		}
+
+		override getHelp()
+		{
+			return "Attack with your manly fisticuffs!"
+		}
+}

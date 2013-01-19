9201600a9ff635cb148c02ed03b5ee567e726aef
Andrew Hanes <ahanes@csh.rit.edu>
2013-01-18 23:05:24 -0500

    Debug stuff and enums

--- server.d
+++ server.d
@@ -2,25 +2,37 @@ import std.stdio;
 import std.socket;
 import core.thread;
 import std.conv;
+import std.string;
 
 class GameServer {
-    void startServer(ushort port = 13373, int bufsize = 1024) {
+
+    enum Consts {
+        DEFAULT_PORT = 13373,
+        DEFAULT_BUFSIZE = 1024,
+        DEFAULT_MAXCONNECTIONS = 8
+    }
+
+    void startServer(ushort port = Consts.DEFAULT_PORT, int bufsize = Consts.DEFAULT_BUFSIZE, int maxcons = Consts.DEFAULT_MAXCONNECTIONS) {
+        debug {
+            writefln("Starting server:\n\tArgs: Port: %d\n\tBuffer Size: %d\n\tMax Connections: %d", port, bufsize, maxcons);
+        }
         auto srv = new TcpSocket;
         srv.bind(new InternetAddress(port));
         srv.listen(10);
-        writefln("Entering loop");
+        debug { writefln("Entering loop"); }
         for(Socket c = srv.accept(); c.isAlive(); c = srv.accept()) {
-            writefln("Waiting for connection...");
-            auto t = new GameThread(c);
+            debug { writefln("Waiting for connection..."); };
+            auto t = new UserThread(c);
             t.start();
         }
     }
 private:
-    class GameThread : Thread {
+    class UserThread : Thread {
         
-        this(Socket rem) {
+        this(Socket rem, int bufs = Consts.DEFAULT_BUFSIZE) {
+            debug { writefln("Thread constructor started"); }
             cli = rem;
-            bufsize = 1024;
+            bufsize = bufs;
             super(&run);
         }
 
@@ -28,27 +40,53 @@ private:
         Socket cli;
         int bufsize;
         void run(){
-            writefln("Running thread...");
+            debug { writefln("Running thread..."); }
             char[] buf = new char[bufsize];
             long n;
-            write("Connection from ");
-            writefln(cli.remoteAddress().toAddrString());
-            writefln("Receiveing");
-            n = cli.receive(buf);
-            writefln("Done receiving");
-            while((n = cli.receive(buf)) != 0) {
-                string s = to!string(buf);
-                writefln("%s", s);
-                cli.send("Ok\n");
+            // n = cli.receive(buf);
+            debug { 
+                write("Connection from ");
+                writefln(cli.remoteAddress().toAddrString());
             }
-            writef("Closing connecting to ");
-            writefln(cli.remoteAddress().toAddrString());
+            debug { writefln("Receiveing"); }
+            n = cli.receive(buf);
+            debug { writefln("Done receiving"); }
+            while(cli.isAlive()) {
+                n = cli.receive(buf);
+                string s = to!string(buf[0..n]);
+                chomp(s, "\n\n");
+                chomp(s, "\r");
+                debug { writefln("%s", s); }
+                cli.send("Ok\n");
+                if(s == GameServerCommands.EXIT) {
+                    break;
+                }
+            }
+            debug {
+                writef("Closing connecting to ");
+                writefln(cli.remoteAddress().toAddrString());
+            }
             cli.close();
         }
     }
 }
 
-void main() {
+// Sent from client
+class GameServerCommands {
+    const string EXIT = "exit";
+}
+
+// Sent from server
+class GameServerStatus {
+    //TODO Fill Out\
+}
+
+int main() {
+    writefln("Starting Gameserver");
+    debug {
+        writefln("***Debug Enabled***");
+    }
     GameServer gs = new GameServer();
     gs.startServer();
+    return 1;
 }

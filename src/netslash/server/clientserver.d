0f39223472f76ce993065fac58bcbf0f578c4701
Andrew Hanes <ahanes@csh.rit.edu>
2013-01-19 15:01:06 -0500

    working

--- src/netslash/server/clientserver.d
+++ src/netslash/server/clientserver.d
@@ -8,23 +8,26 @@ import std.conv;
 class ClientServer {
 private:
     Socket skt;
-    enum Consts {DEFAULT_BUFSIZE = 4096, DEFAULT_PORT = 13373}
+    enum Consts {DEFAULT_BUFSIZE = 2046, DEFAULT_PORT = 13373}
 public:
     this(ushort port = Consts.DEFAULT_PORT, int bufsize = Consts.DEFAULT_BUFSIZE) {
         debug { writefln("Starting client_server\nArgs:\n\tPort: %d\n\tBuffer Size: %d", port, bufsize); }
         writefln(getAddress("hanesmba.student.rit.edu", 13373)[0].toAddrString());
-        debug{writefln("Connecting to %s", addr[0]);}
         skt = new TcpSocket;
         skt.blocking(true);
         //skt.bind(new InternetAddress(InternetAddress.parse("hanesmba.student.rit.edu")[0], Consts.DEFAULT_PORT));
         Socket sock = new TcpSocket(new InternetAddress("hanesmba.student.rit.edu", Consts.DEFAULT_PORT));
-        
+        skt = sock;
         debug { writefln("Entering client loop"); }
     }
 
     string get() {
-        char[] buf;
+        char[] buf = new char[Consts.DEFAULT_BUFSIZE];
         long n = skt.receive(buf);
+
+        debug{ 
+            string str = to!string(buf);
+            writefln("got: %s", str); }
         return to!string(buf[0..n]);
     }
 
